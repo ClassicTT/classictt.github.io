97c398ca86e3d51ab5f54c0aa6ad472a414c9422
Classic Tech Tips <ivoclassic@proton.me>
2025-05-12 16:23:42 +0300

    Update main.js

--- -/scripts/main.js
+++ -/scripts/main.js
@@ -3,3 +3,36 @@ function injectContent() {
 }
 
 injectContent();
+
+
+
+function enlargeImages() {
+	$('img[data-enlargeable]').each(function() {
+		var enlargeable = $(this).attr('data-enlargeable');
+		if (enlargeable === 'false') {
+			return;
+		}
+		$(this).click(function() {
+			var src = $(this).attr('src');
+			var modal;
+
+			function removeModal() {
+				modal.remove();
+				$('body').off('keyup.modal-close');
+			}
+
+			modal = $('<div>').addClass('enlarged-image').css({
+			'background-image': 'url(' + src + ')'
+			}).click(function() {
+			removeModal();
+			}).appendTo('body');
+
+			//ESC
+			$('body').on('keyup.modal-close', function(e) {
+			if (e.key === 'Escape') {
+				removeModal();
+			}
+			});
+		});
+	});
+}
