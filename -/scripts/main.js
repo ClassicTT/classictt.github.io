13d4a722bf9c14db7a84927a826c03426ee99082
ClassicTT <ivoclassic@proton.me>
2025-05-12 14:47:04 +0300

    Update main.js

--- -/scripts/main.js
+++ -/scripts/main.js
@@ -1,3 +1,5 @@
-window.injectContent = function () {
-    document.getElementById('page-footer').innerHTML = "ClassicTT 2025.";
+function injectContent() {
+    document.getElementById('page-footer').innerHTML = "ClassicTT 2025";
 }
+
+injectContent();
